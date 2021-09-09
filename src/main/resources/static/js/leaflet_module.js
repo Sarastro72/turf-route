702402ed3cc5f519342b2281dc44f73a86dbba71
Mattias Bäcklund <mattias.backlund@izettle.com>
2021-09-09 22:49:08 +0200

    Add very basic route info on mouse over.

--- src/main/resources/static/js/leaflet_module.js
+++ src/main/resources/static/js/leaflet_module.js
@@ -5,6 +5,8 @@ let myMap
 let zoneLayer
 let refreshCb
 let llFactor = 1
+let zones = []
+let selectedZone = null
 
 const options = {
     lat: 59.331,
@@ -33,6 +35,7 @@ function initZoneMap(callback) {
 }
 
 function preReDraw() {
+    zones = []
     llFactor = Math.cos(myMap.getCenter().lat * toRad)
     clearZoneMap()
 }
@@ -50,10 +53,10 @@ function getMapBounds() {
 
 }
 
-function drawZone(zone) {
+function drawZone(zone, alpha = 1) {
     let z = L.circle(
         [zone.lat, zone.long],
-        zoneOptionFromZoom()
+        zoneOptionFromZoom(alpha)
     )
     let exits = 0
     if (zone.routes) {
@@ -61,15 +64,14 @@ function drawZone(zone) {
             if (r.weight >= WEIGHT_FILTER) exits++
         })
         z.bindTooltip(zone.name)
-        z.bindPopup(
-            `<b>${zone.name}</b><br>` +
-            `Exits: ${exits}`
-        )
+        z.on('click', function (ev) {
+            selectZone(zone)
+        });
     }
     z.addTo(zoneLayer)
 }
 
-function zoneOptionFromZoom() {
+function zoneOptionFromZoom(alpha = 1) {
     let zoom = myMap.getZoom();
     let opacity = 1
     if (zoom === 12) opacity = 0.66
@@ -80,17 +82,33 @@ function zoneOptionFromZoom() {
         radius: 30 * Math.pow(1.7, 14 - zoom),
         weight: 1,
         color: "#000000",
-        opacity: opacity,
-        fillOpacity: 1,
+        opacity: opacity * alpha,
+        fillOpacity: alpha,
         fillColor: "#ff6644"
     }
 }
 
-function drawRoutes(zone) {
+function drawRoutes(zone, alpha = 1, weightLimit = WEIGHT_FILTER, minWeight = 0) {
+    let count = 0
+    if (zone.routes) {
+        zones.push(zone)
+        count = drawRoutesInternal(zone, alpha, weightLimit, minWeight)
+    }
+    return count
+}
+
+const targetStyle = {
+    color: '#003333',
+    fill: false,
+    weight: 8,
+    opacity: 0
+}
+
+function drawRoutesInternal(zone, alpha = 1, weightLimit = WEIGHT_FILTER, minWeight = 0) {
     let count = 0
     if (zone.routes) {
         zone.routes.forEach(r => {
-            if (r.weight >= WEIGHT_FILTER) {
+            if (r.weight >= weightLimit) {
                 let toPos = {lat: r.toLat, long: r.toLong}
                 const off = delta(zone, toPos, 5)
                 const norm = normal(zone, toPos, 10)
@@ -101,7 +119,21 @@ function drawRoutes(zone) {
                     [zone.lat + off.lat + norm.lat, zone.long + off.long + norm.long],
                     [toPos.lat - off.lat + norm.lat, toPos.long - off.long + norm.long],
                     [toPos.lat, toPos.long]]
-                L.curve(path, getStyleFromWeight(r.weight))
+                let line = L.curve(path, getStyleFromWeight(Math.max(r.weight, minWeight), alpha))
+                    .addTo(zoneLayer)
+                L.curve(path, targetStyle)
+                    .on('mouseover', e => {
+                        e.target.setStyle({opacity: 1})
+                        infoPanel(zone, r)
+                    })
+                    .on('mouseout', e => {
+                        e.target.setStyle({opacity: 0})
+                    })
+                    .on('click', e => {
+                        console.log(zone)
+                        console.log(r)
+                        console.log(`${zone.name} to ${r.toName}, times run ${r.timesRun}, median time ${r.medTime}, fastest time ${r.fastestTime} by ${r.fastestUser}`)
+                    })
                     .addTo(zoneLayer)
                 count++
             }
@@ -110,11 +142,45 @@ function drawRoutes(zone) {
     return count
 }
 
-const alphas = [0, 128, 192, 255, 255, 255, 255, 255, 255, 255, 255]
+function selectZone(zone) {
+    clearZoneMap()
+    if (selectedZone) {
+        selectedZone = null
+        reDrawAll(1)
+    } else {
+        selectedZone = zone.id
+        reDrawZones(0.75)
+        drawRoutes(zone, 1, 0, 3)
+        drawZone(zone)
+    }
+}
+
+function reDrawAll(alpha = 1) {
+    zones.forEach(zone => drawRoutesInternal(zone, alpha))
+    zones.forEach(zone => drawZone(zone, alpha))
+}
+
+function reDrawZones(alpha = 1) {
+    zones.forEach(zone => drawZone(zone, alpha))
+}
+
+function infoPanel(z, r) {
+    const old = document.getElementById("infoPanel");
+    if (old) document.body.removeChild(old);
+
+    const infoPanel = document.createElement("div");
+    infoPanel.id = "infoPanel"
+    infoPanel.style.cssText = 'position:absolute;right: 10px;width:20%;height:20%;opacity:1;z-index:100;background:#ffffff;';
+    infoPanel.innerHTML = `<b>${z.name}</b> to <b>${r.toName}</b><br><b>Times recorded: </b>${r.timesRun}<br>` +
+        `<b>Median time:</b> ${r.medTime}<br><b>Average time:</b> ${r.avgTime}<br><b>Fastest time:</b> ${r.fastestTime} by <b>${r.fastestUser}</b>`;
+    document.body.appendChild(infoPanel);
+}
+
+const alphas = [0, 0.5, 0.75, 1, 1, 1, 1, 1, 1, 1, 1]
 const strokeWeights = [0, 0.3, 0.5, 0.7, 0.9, 1.5, 2, 2.5, 3, 4, 5]
 
-function getStyleFromWeight(w) {
-    const alpha = alphas[w]
+function getStyleFromWeight(w, a = 1) {
+    const alpha = alphas[w] * a
     const weight = strokeWeights[w] + 0.2
     return {
         color: '#000000',
@@ -123,7 +189,6 @@ function getStyleFromWeight(w) {
         opacity: alpha
     }
 }
-
 
 function delta(from, to, frac = 1) {
     return {
